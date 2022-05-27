a15e73cfc366be62c63f4e11ec1c40890db8b780
Chukwuamaka Osuji <osujichukwuamaka@gmail.com>
2022-05-27 11:44:47 +0100

    Update Login component

--- components/Login.tsx
+++ components/Login.tsx
@@ -3,10 +3,12 @@ import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { Card, CardContent, Button, Typography, TextField } from '@mui/material';
 import styles from '../styles/Login.module.css';
-import { makeStyles } from '@material-ui/styles';
-import { authActions } from '../redux/auth-slice';
+import { authActions } from '../redux/slices/auth-slice';
 import { RootState } from '../redux/store';
 import { login } from '../services/login.service';
+import { useState } from 'react';
+import CustomizedSnackbar, { SnackbarState } from './CustomizedSnackbar';
+import Loader from './Loader';
 
 type Input = {
   name: string;
@@ -17,6 +19,8 @@ export default function Login() {
   const router = useRouter();
   const dispatch = useDispatch();
   const loginDetails = useSelector((state: RootState) => state.auth);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [snackbar, setSnackbar] = useState<SnackbarState>({open: false, type: 'info', message: ''});
 
   const inputs: Input[] = [
     {
@@ -37,16 +41,19 @@ export default function Login() {
 
   const handleSubmit = async (e: React.FormEvent<HTMLElement>) => {
     e.preventDefault();
-
+    setLoading(true);
     try {
-      const response = await login(loginDetails);
-      if (response.status === 200) {
-        const data = await response.json();
+      const user = {loginDetails}
+      const response = await login(user);
+      const data = await response.json();
+      if (response.ok) {
         localStorage.setItem('token', data.authentication_token);
+        setLoading(false)
         router.push('/groups');
-      }
+      } else setSnackbar({open: true, type: 'error', message: data.message});
     } catch (err) {
-      console.log(err)
+      setLoading(false);
+      setSnackbar({open: true, type: 'error', message: 'Oops! An error ocurred. Please check your internet and try again'});
     }
   }
 
@@ -59,18 +66,19 @@ export default function Login() {
         </Typography>
         <Typography className={styles.cta} variant='body1' component='p'>Enter your details to get signed in to your account</Typography>
         <form className={styles.form} onSubmit={handleSubmit}>
-          {
-            inputs.map(input => (
-              <TextField
-                key={input.name} className={styles.input} id={input.name} name={input.name}
-                type={input.name} label={input.label} variant="outlined" size='small' required
-                onChange={handleChange}  
-              />
-            ))
-          }
+          {inputs.map(input => (
+            <TextField
+              key={input.name} className={styles.input} id={input.name} name={input.name}
+              type={input.name} label={input.label} variant="outlined" size='small' required
+              onChange={handleChange}  
+            />
+          ))}
           <Button id={styles.login_btn} type='submit' variant='contained'>Log In</Button>
         </form>
       </CardContent>
+
+      <Loader open={loading} />      
+      <CustomizedSnackbar snackbar={snackbar} setSnackbar={setSnackbar} />
     </Card>
   )
 }
