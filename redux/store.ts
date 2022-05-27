01ba15e31986cbcdca9f3a4ec336dda8bf68b841
Chukwuamaka Osuji <osujichukwuamaka@gmail.com>
2022-05-27 09:09:08 +0100

    Add redux store

--- redux/store.ts
+++ redux/store.ts
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { authSlice } from "./auth-slice";
+import { authSlice } from "./slices/auth-slice";
+import { doorSlice } from "./slices/door-slice";
+import { groupSlice } from "./slices/group-slice";
 
 const store = configureStore({
   reducer: {
-    auth: authSlice.reducer
+    auth: authSlice.reducer,
+    groups: groupSlice.reducer,
+    doors: doorSlice.reducer
   }
 })
 
